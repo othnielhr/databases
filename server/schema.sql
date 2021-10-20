ca72cb1859f460e8ce1fad1011ff3573d5a6a094
Othniel Chan <othnielhr.chan@gmail.com>
2021-10-20 16:36:25 -0500

    Complete designing multi-table schema

--- server/schema.sql
+++ server/schema.sql
@@ -1,10 +1,68 @@
+DROP DATABASE IF EXISTS chat;
 CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
+-- CREATE TABLE messages (
   /* Describe your table here.*/
+  -- ---
+-- Globals
+-- ---
+
+-- SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+-- SET FOREIGN_KEY_CHECKS=0;
+
+-- ---
+-- Table 'students'
+--
+-- ---
+
+DROP TABLE IF EXISTS `students`;
+
+CREATE TABLE `students` (
+  `id` INTEGER AUTO_INCREMENT,
+  `name` VARCHAR(12) NULL DEFAULT NULL,
+  `age` INTEGER NULL DEFAULT NULL,
+  `class_id` INTEGER NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
 );
+
+-- ---
+-- Table 'classes'
+--
+-- ---
+
+DROP TABLE IF EXISTS `classes`;
+
+CREATE TABLE `classes` (
+  `id` INTEGER AUTO_INCREMENT,
+  `class` VARCHAR(20) NULL DEFAULT NULL,
+  `teacher` VARCHAR(12) NULL DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
+-- ---
+-- Foreign Keys
+-- ---
+
+ALTER TABLE `students` ADD FOREIGN KEY (class_id) REFERENCES `classes` (`id`);
+
+-- ---
+-- Table Properties
+-- ---
+
+-- ALTER TABLE `students` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+-- ALTER TABLE `classes` ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+-- ---
+-- Test Data
+-- ---
+
+-- INSERT INTO `students` (`id`,`name`,`age`,`class_id`) VALUES
+-- ('','','','');
+-- INSERT INTO `classes` (`id`,`class`,`teacher`) VALUES
+-- ('','','');
+-- );
 
 /* Create other tables and define schemas for them here! */
 
