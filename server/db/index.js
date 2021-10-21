77ca0f04f1bd4f3c0a2c7cf5d590dc2bce9b9c0d
Othniel Chan <othnielhr.chan@gmail.com>
2021-10-20 22:53:46 -0500

    working on customizing tests

--- server/db/index.js
+++ server/db/index.js
@@ -3,5 +3,16 @@ var mysql = require('mysql');
 // Create a database connection and export it from this file.
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
+var connection = mysql.createConnection({
+  user: 'root',
+  database: 'chat'
+});
+connection.connect(function(err) {
+  if (err) {
+    console.log('error connecting', err);
+  } else {
+    console.log('connected as id ', + connection.threadId);
+  }
+});
 
-
+exports.connection = connection;
