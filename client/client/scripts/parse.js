9ce3c7cc3d5f907047b5dcffed8d043c9a45ffaa
Othniel Chan <othnielhr.chan@gmail.com>
2021-10-21 14:04:34 -0500

    modifying client

--- client/client/scripts/parse.js
+++ client/client/scripts/parse.js
@@ -4,7 +4,7 @@
 
 var Parse = {
 
-  server: `https://app-hrsei-api.herokuapp.com/api/chatterbox/messages/${window.CAMPUS}`,
+  server: 'localhost:3000',
 
   create: function(message, successCB, errorCB = null) {
 
